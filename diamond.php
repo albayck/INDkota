1b51aa93c9dc64661b224d0ec26a93b23bcf52bb
albayck <35208924+albayck@users.noreply.github.com>
2019-01-21 15:37:27 +0700

    Update diamond.php

--- diamond.php
+++ diamond.php
@@ -3,7 +3,7 @@
 while(1){
   $ch = curl_init();
 
-  curl_setopt($ch, CURLOPT_URL, "https://api.indkota.com/System/Invite/rewarded_video");
+  curl_setopt($ch, CURLOPT_URL, "https://api.9jta.com/System/Invite/rewarded_video");
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
   curl_setopt($ch, CURLOPT_POSTFIELDS, 
 "member_token=NjIwODU3MjQwNzE1ODEtLXx8LS0wYTIyMjc0YTNiZjBkOGVjOTNlNWMyMmUxNWJhYzMwYw%3D%3D&device_id=66d3a80b14b50f96");
@@ -11,13 +11,13 @@ while(1){
   curl_setopt($ch, CURLOPT_ENCODING, 'gzip, deflate');
 
   $headers = array();
-  $headers[] = "Host: api.indkota.com";
+  $headers[] = "Host: api.9jta.com";
   $headers[] = "Accept: application/json, text/javascript, */*; q=0.01";
   $headers[] = "Origin: file://";
   $headers[] = "User-Agent: Mozilla/5.0 (Linux; Android 7.0; Redmi Note 4 Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/70.0.3538.80 Mobile Safari/537.36";
   $headers[] = "Content-Type: application/x-www-form-urlencoded";
   $headers[] = "Accept-Language: id-ID,en-US;q=0.9";
-  $headers[] = "X-Requested-With: com.tahu.fep";
+  $headers[] = "X-Requested-With: com.indkota.fep";
   curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 
   $result = curl_exec($ch);
